403fce27657352b147c6289e6ac20a6fc0b8e8ae
maliiayik <maliiayik@gmail.com>
2024-04-12 23:05:13 +0300

    CodeFirst complated.

---
+++ EFCore.CodeFirst/DAL/AppDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCore.CodeFirst.DAL
+{
+    public class AppDbContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //console uygulaması oldugu için burada build metodunu cagırıyoruz .net veya api projelerinde buna gerek yok.Migration sırasında Connection stringi okuması için  o yüzden build metodunu çağırıyoruz.
+            Initializer.Build();
+            optionsBuilder.UseSqlServer(Initializer.Configuration.GetConnectionString("SqlCon"));
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ EFCore.CodeFirst/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using EFCore.CodeFirst;
+using EFCore.CodeFirst.DAL;
+using Microsoft.EntityFrameworkCore;
+
+Initializer.Build();
+using (var context = new AppDbContext())
+{
+    var product =  await context.Products.ToListAsync();
+
+    product.ForEach(p =>
+    {
+        Console.WriteLine($"{p.Name} - {p.Price} - {p.Stock}");
+    });
+}
