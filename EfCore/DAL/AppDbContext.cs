e4a9562ed66cb6e51b07e337d3d477c6e6c6eb3a
maliiayik <maliiayik@gmail.com>
2024-04-29 23:41:01 +0300

    done

--- EfCore/DAL/AppDbContext.cs
+++ EfCore/DAL/AppDbContext.cs
@@ -9,14 +9,25 @@ namespace EfCore.DAL
 {
     public class AppDbContext:DbContext
     {
+        public DbSet<BasePerson> Persons { get; set; }
+        public DbSet<Manager> Managers { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer("Data Source=DESKTOP-KVIRVD3\\SQLEXPRESS;Initial Catalog=EFCoreDbContextDb;Integrated Security=True;Connect Timeout=30;Encrypt=True;Trust Server Certificate=True;Application Intent=ReadWrite;Multi Subnet Failover=False");
+            optionsBuilder.UseSqlServer("Data Source=DESKTOP-KVIRVD3\\SQLEXPRESS;Initial Catalog=EFCoreInheritanceDB;Integrated Security=True;Connect Timeout=30;Encrypt=True;Trust Server Certificate=True;Application Intent=ReadWrite;Multi Subnet Failover=False");
         }
 
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            //TPT
+            modelBuilder.Entity<BasePerson>().ToTable("Persons");
+            modelBuilder.Entity<Employee>().ToTable("Employees");
+            modelBuilder.Entity<Manager>().ToTable("Managers");
+
+
             base.OnModelCreating(modelBuilder);
         }
     }
