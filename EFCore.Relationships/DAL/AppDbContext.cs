8aca687e6f5c5b1cd94b8d444fcff1e7b45fa234
maliiayik <maliiayik@gmail.com>
2024-05-09 21:07:20 +0300

    done

--- EFCore.Relationships/DAL/AppDbContext.cs
+++ EFCore.Relationships/DAL/AppDbContext.cs
@@ -16,45 +16,14 @@ namespace EFCore.Relationships.DAL
 
         public DbSet<Product> Products { get; set; }
         public DbSet<Category> Categories { get; set; }
-        //public DbSet<ProductFeature> ProductFeatures { get; set; }
+        public DbSet<ProductFeature> ProductFeatures { get; set; }
 
         //public DbSet<Teacher> Teachers { get; set; }
         //public DbSet<Student> Students { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<Product>().Property(x => x.TotalAmount).HasComputedColumnSql("[Price]*[Kdv]");
-
-
-            modelBuilder.Entity<Product>().Property(x => x.Id).ValueGeneratedNever();//None
-            modelBuilder.Entity<Product>().Property(x => x.TotalAmount).ValueGeneratedOnAddOrUpdate();//Computed
-            modelBuilder.Entity<Product>().Property(x => x.TotalAmount).ValueGeneratedOnAdd();//Identity
-
-
-
-
-
-
-
-
-            //one-to-one
-            //modelBuilder.Entity<Product>().HasOne(x=> x.ProductFeature).WithOne(x=>x.Product).HasForeignKey<ProductFeature>(x=>x.ProductId);
-
-            //one-to-many
-            //modelBuilder.Entity<Category>().HasMany(x=> x.Products).WithOne(y=> y.Category).HasForeignKey(x=>x.CategoryId);
-
-            //one-to-one
-            //modelBuilder.Entity<Product>().HasOne(x => x.ProductFeature).WithOne(x => x.Product).HasForeignKey<ProductFeature>(x => x.Id);
-
-            //many to many
-            //modelBuilder.Entity<Student>()
-            //    .HasMany(x => x.Teachers)
-            //    .WithMany(x => x.Students)
-            //    .UsingEntity<Dictionary<string, object>>(
-            //    "StudentTeacherTb",
-            //    x => x.HasOne<Teacher>().WithMany().HasForeignKey("Teacher_ID").HasConstraintName("FK_TeacherId"),
-            //    y => y.HasOne<Student>().WithMany().HasForeignKey("Student_Id").HasConstraintName("FK_StudentId")
-            //    ); ;
+            
 
             base.OnModelCreating(modelBuilder);
         }
--- EFCore.Relationships/Program.cs
+++ EFCore.Relationships/Program.cs
@@ -1,11 +1,48 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using EFCore.Relationships.DAL;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure.Internal;
 
 Console.WriteLine("Hello, World!");
 using (var context = new AppDbContext())
 {
+    //isimsiz yansıtma 1. örnek
+    var products = context.Products.Include(x => x.Category).Select(x => new
+    {
+        //Annonymous (isimsiz) bir yansıtma  yapmış olduk.
+        CategoryName = x.Category.Name,
+        ProductName = x.Name,
+        ProductPrice = x.Price
+    }).Where(x => x.ProductPrice > 100).ToList();
 
+
+
+    //isimsiz yansıtma 2. örnek
+
+    var products2 = context.Categories.Include(x => x.Products).ThenInclude(x => x.ProductFeature).Select(x => new
+    {
+        //burada yine isimsiz bir yansıtma yapıyoruz ve yansıtmanın içinde join select gibi linq sorguları yazabiliyoruz.
+
+        CategoryName = x.Name,
+        ProductName = String.Join(",", x.Products.Select(x => x.Name)),//product nameleri virgülle                                                         ayrılmış bir şekilde yazdırmış olduk.
+        TotalPrice = x.Products.Sum(x => x.Price)
+
+        //buradaki data where şartında yazmıs oldugumuz değerlerle birlikte bize sonuç döndürür.
+        //yani where şartını sonradan oluşturmaz hepsini birlikte gerçekleştirir.
+    }).Where(y => y.TotalPrice > 100).OrderBy(x => x.TotalPrice).ToList();
+
+
+    //isimsit yansıtma örnek 3
+
+    //Eğerki İsimsiz yansıtma yapacağımız entityler içerisinde  navigation propertyler var ise include kullanmamız gerekmemektedir, select ifadesi ile bu propertylere erişebiliriz.
+    var products = context.Products.Select(x => new
+    {
+        //Annonymous (isimsiz) bir yansıtma  yapmış olduk.
+        CategoryName = x.Category.Name,
+        ProductName = x.Name,
+        ProductPrice = x.Price,
+        ProductColor=x.ProductFeature.Color
+    }).Where(x => x.ProductPrice > 100).ToList();
 
 
 }
